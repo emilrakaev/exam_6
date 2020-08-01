2b2c140800f1a3c5a5aa31a113b092152b9a2173
Emil <emilrakaev@gmail.com>
2020-08-01 12:10:24 +0600

    Модель зарегестрирована в админке

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Guest
 
-# Register your models here.
+
+class GuestAdmin(admin.ModelAdmin):
+    list_filter = ('status',)
+    list_display = ('pk', 'name', 'email', 'text', 'create_at', 'update_at')
+    list_display_links = ('pk', 'name')
+    search_fields = ('name',)
+
+
+admin.site.register(Guest, GuestAdmin)
