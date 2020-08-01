a2a0e5427614df277b497d399515ddc2df7582a9
Emil <emilrakaev@gmail.com>
2020-08-01 13:24:18 +0600

    Создал страницу добавления, частичные шаблоны

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseNotAllowed
 from webapp.models import Guest
-# from webapp.forms import GuestForm
+from webapp.forms import GuestForm
 
 
 def index_view(request):
@@ -9,3 +9,23 @@ def index_view(request):
     return render(request, 'index.html', context={
         'guests': data
     })
+
+
+def guest_create_view(request):
+    if request.method == 'GET':
+        return render(request, 'guest_create.html', context={
+            'form': GuestForm()
+        })
+    elif request.method == 'POST':
+        form = GuestForm(data=request.POST)
+        if form.is_valid():
+            guest = Guest.objects.create(
+                name=form.cleaned_data['name'],
+                email=form.cleaned_data['email'],
+                text=form.cleaned_data['text']
+            )
+            return redirect('index')
+        else:
+            return render(request, 'guest_create.html', context={
+                'form': form
+            })
